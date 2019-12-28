b0ec24ae6571ba0e7942be41131da0893ff4a709
rob <robert.simons@wsu.edu>
2019-12-27 22:34:31 -0600

    Initial Stopwatch

---
+++ C#/Stopwatch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Watch.Start();
+            Watch.End();
+            Console.WriteLine("Duration 1: " + Watch.Duration);
+
+            Watch.Start();
+            Watch.End();
+            Console.WriteLine("Duration 2: " + Watch.Duration);
+        }
+    }
+}
---
+++ C#/Stopwatch/Watch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stopwatch
+{
+    class Watch
+    {
+        private static DateTime? _start = null;
+        private static DateTime _end;
+
+        public static TimeSpan Duration { get; private set; }
+        public static void Start()
+        {
+            if (_start.HasValue)
+            {
+                throw new InvalidOperationException("Watch is already started");
+            }
+
+            _start = DateTime.Now;
+        }
+
+        public static void End()
+        {
+            if (!_start.HasValue)
+            {
+                throw new InvalidOperationException("Watch has not been started");
+            } else
+            {
+                _end = DateTime.Now;
+                Duration = _end - (DateTime)_start;
+                _start = null;
+            }
+        }
+    }
+}
