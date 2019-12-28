52a548ff215aff9f5942827554aea19f6884bac5
rob <robert.simons@wsu.edu>
2019-12-27 23:16:49 -0600

    Initial MessageBoard

---
+++ C#/MessageBoard/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MessageBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post1 = new Post("Idealism of posting without a description");
+            post1.Upvote();
+            post1.Upvote();
+            post1.Downvote();
+            post1.Upvote();
+            post1.Downvote();
+            post1.Upvote();
+            post1.Upvote();
+
+            Console.WriteLine("Current vote: " + post1.Vote);
+        }
+    }
+}
