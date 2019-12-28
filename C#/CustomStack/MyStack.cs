855da36eb6c51c46520f3b35b1dc8b7435ef8ff4
rob <robert.simons@wsu.edu>
2019-12-28 00:20:19 -0600

    Initial CustomStack

---
+++ C#/CustomStack/MyStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace CustomStack
+{
+    class MyStack
+    {
+        private Stack _stack = new Stack();
+
+        public MyStack()
+        {
+            // Intentionally left blank
+        }
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("Cannot push null onto the stack.");
+            }
+
+            _stack.Push(obj);
+        }
+
+        public object Pop()
+        {
+            if (_stack.Count == 0)
+            {
+                throw new InvalidOperationException("Cannot pop from an empty stack");
+            }
+
+            return _stack.Pop();
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+    }
+}
